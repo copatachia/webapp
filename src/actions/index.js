64001371834ee0c124a9a818b30101024974cd2d
copatachia <copatachia@pentalog.com>
2018-10-17 15:34:56 +0300

    not finished yet

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,18 @@
-const API_KEY = '54f3991b05cb1fb4d5384f673650a508';
+import axios from 'axios';
 
-//v3 from git repo
+const API_KEY = '54f3991b05cb1fb4d5384f673650a508';
+const rootURL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
+
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+
+export function fetchWeather(city){
+    const url = `${rootURL}&q=${city},ro`;
+
+    const request = axios.get(url);
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
