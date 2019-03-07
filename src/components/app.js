8024ff80e324580e18b5fb6c1aa841c95227c9b2
copatachia <copatachia@pentalog.com>
2019-03-07 14:21:02 +0200

    terminati-va

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import SearchBar from '../containers/search_bar';
 
-
+//terminati-va ma
 //test
 export default class App extends Component {
   render() {
